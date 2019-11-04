6322e6a346331594abaa18be5857706d5d472958
azrap <azraap@gmail.com>
2019-11-04 15:42:11 -0800

    finished merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,33 +1,44 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
+def merge(arrA, arrB):
+    merged_arr = []
+    i = 0
+    j = 0
+    while i < len(arrA) and j < len(arrB):
+        if(arrA[i] > arrB[j]):
+            merged_arr.append(arrB[j])
+            j += 1
+        else:
+            merged_arr.append(arrA[i])
+            i += 1
+
+    if i < len(arrA):
+        merged_arr.extend(arrA[i:len(arrA)])
+    elif j < len(arrB):
+        merged_arr.extend(arrB[j:len(arrB)])
+
     return merged_arr
 
 
+# print(merge([0, 1, 2], [1, 2, 5, 30]))
+
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
-
-    return arr
 
 
-# STRETCH: implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # TO-DO
+def merge_sort(arr):
 
-    return arr
+    # base case
 
-def merge_sort_in_place(arr, l, r): 
-    # TO-DO
+    print(arr)
+    if len(arr) <= 1:
+        return arr
 
-    return arr
+    mid = (len(arr)//2)  # the double slash floors the length
+    arr1 = arr[0:mid]
+    arr2 = arr[mid:]
+
+    return merge(merge_sort(arr1), merge_sort(arr2))
+
+    # print("arr: ", arr)
 
 
-# STRETCH: implement the Timsort function below
-# hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
-
-    return arr
+print(merge_sort([4, 2, 5, 1]))
